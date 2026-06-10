7b2db6d3974133609dd22949393b55eeb35d5489
Cesar <user.email>
2026-06-10 09:03:51 -0600

    Estructura base de los mapas

---
+++ mapas.h
@@ -0,0 +1,11 @@
+#ifndef MAPAS_H
+#define MAPAS_H
+
+#define FILAS_MAPA 60
+#define COLUMNAS_MAPA 60
+
+extern char nivel1[FILAS_MAPA][COLUMNAS_MAPA];
+extern char nivel2[FILAS_MAPA][COLUMNAS_MAPA];
+extern char nivel3[FILAS_MAPA][COLUMNAS_MAPA];
+
+#endif
