7c7cdf3d40110165c944b4413810f8e97bf8b657
Cesar <user.email>
2026-06-10 09:09:47 -0600

    Información de niveles

--- mapas.h
+++ mapas.h
@@ -8,4 +8,17 @@ extern char nivel1[FILAS_MAPA][COLUMNAS_MAPA];
 extern char nivel2[FILAS_MAPA][COLUMNAS_MAPA];
 extern char nivel3[FILAS_MAPA][COLUMNAS_MAPA];
 
+extern int filaInicioNivel1;
+extern int columnaInicioNivel1;
+
+extern int filaInicioNivel2;
+extern int columnaInicioNivel2;
+
+extern int filaInicioNivel3;
+extern int columnaInicioNivel3;
+
+extern int totalMonedasNivel1;
+extern int totalMonedasNivel2;
+extern int totalMonedasNivel3;
+
 #endif
