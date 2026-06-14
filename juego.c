7f287bb68d1454fab0b66aa07a84142a08943fa6
Emiliano <emilianoriosmunoz14@gmail.com>
2026-06-14 14:01:14 -0600

    Implementacion juego.c

--- juego.c
+++ juego.c
@@ -1,10 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "juego.h"
 /*
  * mover_jugador
  * Procesa una tecla WASD y actualiza el estado del juego.
  * Retorna 1 si el jugador llego a la salida (E), 0 en cualquier otro caso.
  */
-int mover_jugador(
+void buscar_jugador(char mapa[FILAS_MAPA][COLUMNAS_MAPA], int *fila, int *col)
+{
+    for (int i = 0; i < FILAS_MAPA; i++) {
+        for (int j = 0; j < COLUMNAS_MAPA; j++) {
+            if (mapa[i][j] == 'P') {
+                *fila = i;
+                *col  = j;
+                return;
+            }
+        }
+    }
+    *fila = 1;
+    *col  = 1;
+}
+
+void imprimir_ventana(char mapa[FILAS_MAPA][COLUMNAS_MAPA], EstadoJuego *estado)
+{
+    system("cls");
+
+    int cam_fila = estado->jugador_fila - VENTANA_FILS / 2;
+    int cam_col  = estado->jugador_col  - VENTANA_COLS / 2;
+
+    if (cam_fila < 0) cam_fila = 0;
+    if (cam_col  < 0) cam_col  = 0;
+    if (cam_fila > FILAS_MAPA    - VENTANA_FILS) cam_fila = FILAS_MAPA    - VENTANA_FILS;
+    if (cam_col  > COLUMNAS_MAPA - VENTANA_COLS) cam_col  = COLUMNAS_MAPA - VENTANA_COLS;
+
+    for (int i = 0; i < VENTANA_FILS; i++) {
+        for (int j = 0; j < VENTANA_COLS; j++) {
+            int mf = cam_fila + i;
+            int mc = cam_col  + j;
+
+            if (mf == estado->jugador_fila && mc == estado->jugador_col)
+                printf("P");
+            else
+                printf("%c", mapa[mf][mc]);
+        }
+        printf("\n");
+    }
+
+    int total = (estado->nivel == 1) ? totalMonedasNivel1 :
+                (estado->nivel == 2) ? totalMonedasNivel2 :
+                                       totalMonedasNivel3;
+    imprimir_stats(estado, total);
+}
+
+void imprimir_stats(EstadoJuego *estado, int total_monedas)
+{
+    printf("-----------------------------\n");
+    printf("Nivel  : %d\n",  estado->nivel);
+    printf("Monedas: %d / %d\n", estado->monedas, total_monedas);
+    printf("Pasos  : %d\n",  estado->pasos);
+    printf("Llave  : %s\n",  estado->llave ? "Si" : "No");
+    printf("-----------------------------\n");
+    printf("Movimiento: W A S D  |  Salir: Q\n");
+}
+
+ int mover_jugador(
     char mapa[FILAS_MAPA][COLUMNAS_MAPA],
     EstadoJuego *estado,
     char tecla
